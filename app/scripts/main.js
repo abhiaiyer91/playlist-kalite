e765faebdafff92475b326ab7671facc545295e1
Abhi Aiyer <abhiaiyer91@gmail.com>
2014-05-02 14:52:50 -0700

    updated resos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,3 @@
- 
-
-
-(function () {
-
-
-var timer;
-
-$(window).on('mousemove', function () {
-  $('nav').addClass('show');
-  try {
-    clearTimeout(timer);
-  } catch (e) {}
-  timer = setTimeout(function () {
-    $('nav').removeClass('show');
-  }, 1500);
-});
-
-    $('.item').click(function (e){
-        if($(this).next('.item-data').css('display') != 'block'){
-            $('.active').slideUp('fast').removeClass('active');
-            $(this).next('.item-data').addClass('active').slideDown('slow');
-        } else {
-            $('.active').slideUp('fast').removeClass('active');
-        }
-    });
-
-})()
 
 
 
